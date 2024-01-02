0398e2f32942241b12f1101bf4e095b4005cfafc
Jeroen van Erp <jvanerp@stackstate.com>
2024-01-02 17:03:10 +0100

    Make stackstateprocessor process all telemetry types
    Signed-off-by: Jeroen van Erp <jvanerp@stackstate.com>

--- processor/stackstateprocessor/config.go
+++ processor/stackstateprocessor/config.go
@@ -1,15 +1,24 @@
 package stackstateprocessor
 
-import "fmt"
+import (
+	"fmt"
+
+	"go.opentelemetry.io/collector/config/configopaque"
+)
 
 type Config struct {
 	// The ClusterName to tag the trace with
-	ClusterName string `mapstructure:"cluster_name"`
+	ClusterName string              `mapstructure:"cluster_name"`
+	ApiKey      configopaque.String `mapstructure:"api_key"`
 }
 
 func (c *Config) Validate() error {
 	if c.ClusterName == "" {
 		return fmt.Errorf("cluster_name is required")
+	}
+
+	if c.ApiKey == "" {
+		return fmt.Errorf("api_key is required")
 	}
 
 	return nil
