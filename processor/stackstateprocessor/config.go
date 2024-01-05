43ca4ce70cecd30453a5e476e103b6be9ddd500a
Jeroen van Erp <jvanerp@stackstate.com>
2024-01-05 16:27:15 +0100

    Remove api key from stackstate processor

--- processor/stackstateprocessor/config.go
+++ processor/stackstateprocessor/config.go
@@ -2,23 +2,16 @@ package stackstateprocessor
 
 import (
 	"fmt"
-
-	"go.opentelemetry.io/collector/config/configopaque"
 )
 
 type Config struct {
 	// The ClusterName to tag the trace with
-	ClusterName string              `mapstructure:"cluster_name"`
-	ApiKey      configopaque.String `mapstructure:"api_key"`
+	ClusterName string `mapstructure:"cluster_name"`
 }
 
 func (c *Config) Validate() error {
 	if c.ClusterName == "" {
 		return fmt.Errorf("cluster_name is required")
-	}
-
-	if c.ApiKey == "" {
-		return fmt.Errorf("api_key is required")
 	}
 
 	return nil
