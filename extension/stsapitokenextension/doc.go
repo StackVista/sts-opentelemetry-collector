28688005471ea207ba8ddf28b928e9a74a4732f0
Remco Beckers <rbeckers@stackstate.com>
2024-04-24 10:30:58 +0200

    Remove unused code

--- extension/stsapitokenextension/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-// The STS API Token Extension is used as a client side credential provider to connect to StackState's OTEL ingestion API's.
-//
-//go:generate mdatagen metadata.yaml
-package stsapitokenextension
--- processor/stackstateprocessor/doc.go
+++
@@ -1,5 +0,0 @@
-// The StackState Processor is a custom processor that adds StackState specific attributes to the trace, log or metric.
-// It is intended to be used in conjunction with the StackState Exporter, so that the right attributes are added to the trace and StackState can correctly link the data to the Kubernetes resources.
-//
-//go:generate mdatagen metadata.yaml
-package stackstateprocessor
