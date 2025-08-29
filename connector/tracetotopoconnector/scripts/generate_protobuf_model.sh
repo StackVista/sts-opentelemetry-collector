2b8f95f9dcba7db8dc7b38f83adb5eceac62d1a1
Lukasz Marchewka <lukasz.marchewka@stackstate.com>
2025-08-29 08:36:19 +0200

    STAC-23198 Download and generate protobuf mobel

--- connector/tracetotopoconnector/scripts/generate_protobuf_model.sh
+++ connector/tracetotopoconnector/scripts/generate_protobuf_model.sh
@@ -3,20 +3,21 @@
 set -e && cd "$(dirname "$0")" && cd ..
 echo `pwd`
 
-OPENAPI_VERSION=$(cat topostream_version)
+PROTOBUF_VERSION=$(cat topostream_version)
 CHECKOUT_DIR="checkout"
 
 rm -rf "$CHECKOUT_DIR"
 
 # In gitlab we authenticate with the job token when cloning
-if [ -z "${CI_JOB_TOKEN}" ]; then
+if [ -z "${GITLAB_READ_TOKEN}" ]; then
   git clone git@gitlab.com:stackvista/platform/shared-protobuf-protocols.git "$CHECKOUT_DIR"
 else
   git clone "https://stackstate-system-user:${GITLAB_READ_TOKEN}@gitlab.com/stackvista/platform/shared-protobuf-protocols.git" "$CHECKOUT_DIR"
 fi
 
-git -C "$CHECKOUT_DIR" checkout "$OPENAPI_VERSION"
+git -C "$CHECKOUT_DIR" checkout "$PROTOBUF_VERSION"
 cp "$CHECKOUT_DIR/topo_stream/v1/topo_stream.proto" "spec/topo_stream.proto"
 rm -rf "$CHECKOUT_DIR"
 
+export PATH="$PATH:$(go env GOPATH)/bin"
 go generate ./generated/topostream/model.go
