a189ea52118a2297fde094fe60f2d226bd40fe18
Lukasz Marchewka <lukasz.marchewka@stackstate.com>
2025-08-29 14:56:35 +0200

    STAC-23191 Check if there is no changes in generated code

---
+++ scripts/check_no_changes.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Check whether there are any changes to source files.
+# If so, this means some code did not get committed.
+git add -A
+if [[ $( git diff --cached --name-only ) ]]; then
+  printf -- "There were changes in the working directory!\n"
+  printf -- "This means some cached files should have been committed but were not\n"
+  git diff --cached --name-only
+  exit 1
+else
+  printf -- "No changes detected on working directory!\n\n"
+fi
