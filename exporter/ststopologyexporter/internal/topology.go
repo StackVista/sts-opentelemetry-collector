0cee269241cc05f1c938e7d23455bd853c1adb6e
Alejandro Acevedo <alejandro.acevedo@suse.com>
2025-10-23 15:29:52 +0200

    STAC-23660: Add missing cluster and namespace tags

--- exporter/ststopologyexporter/internal/topology.go
+++ exporter/ststopologyexporter/internal/topology.go
@@ -187,8 +187,6 @@ func (c *ComponentsCollection) addKubernetesRelation(attrs *pcommon.Map, instanc
 		newComponentData().
 			withNameFromAttr(attrs, "k8s.pod.name").
 			withScope(attrs).
-			withTagValue("cluster-name", reqAttrs["k8s.cluster.name"]).
-			withTagValue("namespace", reqAttrs["k8s.namespace.name"]).
 			withIdentifier(fmt.Sprintf(
 				"urn:kubernetes:/%s:%s:pod/%s",
 				reqAttrs["k8s.cluster.name"],
@@ -411,8 +409,11 @@ func (c *ComponentData) withTagPrefix(attrs *pcommon.Map, prefix string) *Compon
 
 func (c *ComponentData) withScope(attrs *pcommon.Map) *ComponentData {
 	clusterName, ok := attrs.Get("k8s.cluster.name")
+
 	if ok {
+		c.Tags["cluster-name"] = clusterName.AsString()
 		namespaceName, nok := attrs.Get("k8s.namespace.name")
+		c.Tags["namespace"] = namespaceName.AsString()
 		if nok {
 			c.Tags["k8s-scope"] = fmt.Sprintf("%s/%s", clusterName.AsString(), namespaceName.AsString())
 		}
