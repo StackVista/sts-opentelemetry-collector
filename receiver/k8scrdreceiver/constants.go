2b9469b915075f02b0c016a49b73398a313280f5
Deon Taljaard <deon.taljaard@suse.com>
2026-04-08 17:32:30 +0200

    STAC-24283: add custom k8scrdreceiver
    # Conflicts:
    #	test/e2e/go.mod

---
+++ receiver/k8scrdreceiver/constants.go
@@ -0,0 +1,17 @@
+package k8scrdreceiver
+
+const (
+	// scopeName is the instrumentation scope name for logs emitted by this receiver
+	scopeName = "github.com/stackvista/sts-opentelemetry-collector/receiver/k8scrdreceiver"
+
+	// Attribute keys for CR log records
+	attrK8sResourceName    = "k8s.resource.name"
+	attrK8sResourceGroup   = "k8s.resource.group"
+	attrK8sResourceVersion = "k8s.resource.version"
+	attrEventDomain        = "event.domain"
+	attrEventName          = "event.name"
+	attrK8sNamespaceName   = "k8s.namespace.name"
+
+	// eventDomainK8s is the value for the event.domain attribute
+	eventDomainK8s = "k8s"
+)
