2cb95ecf86110f6e6607f647dd0b7e91c5f9a0af
Deon Taljaard <deon.taljaard@suse.com>
2026-04-08 17:32:30 +0200

    STAC-24283: add custom k8scrdreceiver
    # Conflicts:
    #	test/e2e/go.mod

---
+++ receiver/k8scrdreceiver/factory.go
@@ -0,0 +1,52 @@
+package k8scrdreceiver
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+
+	"github.com/stackvista/sts-opentelemetry-collector/receiver/k8scrdreceiver/internal/metadata"
+)
+
+// NewFactory creates a factory for k8scrd receiver.
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		receiver.WithLogs(
+			createLogsReceiver,
+			metadata.LogsStability,
+		),
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		APIConfig: APIConfig{
+			AuthType: AuthTypeServiceAccount,
+		},
+		DiscoveryMode:       DiscoveryModeAPIGroups,
+		IncludeInitialState: true, // Emit existing CRs on startup by default
+		APIGroupFilters: &APIGroupFilters{
+			Include: []string{"*"}, // Watch all API groups by default
+			Exclude: []string{},
+		},
+	}
+}
+
+func createLogsReceiver(
+	_ context.Context,
+	params receiver.Settings,
+	cfg component.Config,
+	consumer consumer.Logs,
+) (receiver.Logs, error) {
+	rcfg, ok := cfg.(*Config)
+	if !ok {
+		return nil, fmt.Errorf("invalid config type: %T", cfg)
+	}
+
+	return newReceiver(params, rcfg, consumer)
+}
